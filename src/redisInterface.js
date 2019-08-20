cf2b713bad4bd7214ba77a616f5c8d623dabba4d
Artie Gold <the.artiegold@gmail.com>
2019-08-19 20:02:05 -0500

    oops

---
+++ src/redisInterface.js
@@ -0,0 +1,34 @@
+const asyncRedis = require("async-redis");
+let client;
+
+async function init() {
+  client = await asyncRedis.createClient();
+  client.on("error", error => console.error("redis error: " + error));
+  await client.flushall();
+}
+
+async function pushItem(bucket, link) {
+  await client.zadd("bucketList", bucket, bucket);
+  await client.sadd(bucket, link);
+}
+
+async function result(f) {
+  let indicator = null;
+  while (indicator !== "0") {
+    const bucketsRaw = await client.zscan("bucketList", 0);
+    indicator = bucketsRaw[0];
+    const bucket2s = Array.from(new Set(bucketsRaw[1]));
+    bucket2s.forEach(async k => {
+      const values = await client.smembers(k);
+      values.forEach(v => f(k,v));
+    });
+    console.log(indicator);
+  }
+  return true;
+}
+
+module.exports = {
+  init,
+  pushItem,
+  result
+};
